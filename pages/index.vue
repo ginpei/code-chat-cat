079cf6e6a1f8ebb076c6d1d930f1c39e69d6fe04
ginpei <ginpei@gmail.com>
2018-06-09 01:34:23 -0700

    home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,58 @@
-<template>
-  <section class="container">
-    <div>
-      <a href="/rooms/777">
-        <app-logo/>
-      </a>
-    </div>
-  </section>
+<template lang="pug">
+  div.container
+    div.logo
+      img.logo-image(src="~assets/logo-512.png" width="256" height="256" alt="Face of Code-Class-Chat Cat")
+    h1 Classes
+    ul
+      li(v-for="room in rooms")
+        a(:href="roomLink(room)") {{ room.title }}
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import firebase from '~/plugins/firebase.js';
+import { mapState, mapGetters, mapActions } from 'vuex';
 
 export default {
-  components: {
-    AppLogo
+  computed: {
+    ...mapState({
+      rooms: 'rooms',
+    }),
+  },
+
+  created () {
+    this.$store.dispatch('setRoomsRef', firebase.database().ref('rooms'));
+  },
+
+  methods: {
+    roomLink (room) {
+      if (!room) {
+        throw new Error('Room must be given');
+      }
+      return `/rooms/${room['.key']}`;
+    }
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+  // min-height: 100vh;
+  // min-width: 512px;
+  // padding: 20px;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+}
+
+.logo {
   text-align: center;
+
+  .logo-image {
+    &:hover {
+      transform: scale(1.01);
+    }
+  }
 }
 </style>
