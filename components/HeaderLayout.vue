e9580074f9cc658317f411fa481a2f7325c7af88
ginpei <ginpei@gmail.com>
2018-06-24 01:04:11 -0700

    links in header

--- components/HeaderLayout.vue
+++ components/HeaderLayout.vue
@@ -3,7 +3,12 @@
     header.defaultLayout-header
       div.globalHeader
         a.globalHeader-logo(v-if="titleUrl" :href="titleUrl") {{ title || defaultTitle }}
-        span.globalHeader-logo(v-if="!titleUrl") {{ title || defaultTitle }}
+        span.globalHeader-logo(v-else) {{ title || defaultTitle }}
+        div.globalHeaderMenu
+          div.globalHeaderMenu-group(v-for="link in links")
+            span.globalHeaderMenu-title {{ link.title }}
+            div.globalHeaderMenu-list
+              a.globalHeaderMenu-item(v-for="item in link.items" :href="item.href") {{ item.title }}
     div.defaultLayout-body
       slot(v-if="!processing || !firstTime")
     footer.defaultLayout-footer(v-if="!noFooter")
@@ -23,6 +28,7 @@ export default {
     'title',
     'titleLink',
     'noFooter',
+    'links'
   ],
 
   components: {
@@ -71,9 +77,11 @@ export default {
 }
 
 .globalHeader {
+  justify-content: space-between;
   background-color: #036;
   box-sizing: border-box;
   color: #fff;
+  display: flex;
   font-size: 0.8rem;
   height: var(--defaultLayout-headerHeight);
   left: 0;
@@ -82,13 +90,62 @@ export default {
   top: 0;
   width: 100%;
 
-  a.globalHeader-logo {
+  a {
     color: inherit;
     text-decoration: none;
 
     &:hover {
       text-decoration: underline;
     }
+  }
+}
+
+.globalHeaderMenu {
+  cursor: default;
+  display: flex;
+
+  .globalHeaderMenu-group {
+    background-color: #036;
+    margin-left: 0em;
+    padding: 0 1px;
+    position: relative;
+
+    &:hover {
+      border: 1px silver;
+      border-style: solid solid none;
+      padding: 0;
+
+      .globalHeaderMenu-list {
+        border: 1px solid silver;
+        height: auto;
+        opacity: 1;
+        padding-top: 0.5em;
+        transition: opacity 0;
+      }
+    }
+  }
+
+  .globalHeaderMenu-title {
+    padding: 0 0.5em;
+  }
+
+  .globalHeaderMenu-list {
+    background-color: #036;
+    box-shadow: 0 0 20px #0006;
+    height: 0;
+    opacity: 0;
+    overflow: hidden;
+    position: absolute;
+    right: -1px;
+    top: calc(1.4em - 1px);
+    transition: opacity 200ms;
+    min-width: 5em;
+    z-index: -1;
+  }
+
+  .globalHeaderMenu-item {
+    display: block;
+    padding: 0.4em 1em;
   }
 }
 
--- pages/rooms/_id/manage.vue
+++ pages/rooms/_id/manage.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  HeaderLayout(:processing="processing")
+  HeaderLayout(:processing="processing" :links="headerLinks")
     div.container
       h1 Manage your class room
       h2 URLs
@@ -57,6 +57,26 @@ export default {
   },
 
   computed: {
+    headerLinks () {
+      return [
+        {
+          title: 'Room',
+          items: [
+            { title: 'Home', href: this.roomUrlOf(this.roomId) },
+            { title: 'Manage', href: this.roomUrlOf(this.roomId, 'manage') },
+            { title: 'Edit', href: this.roomUrlOf(this.roomId, 'edit') },
+          ],
+        },
+        {
+          title: this.userName,
+          items: [
+            { title: 'My rooms', href: '/rooms/' },
+            { title: 'Sign out', href: '/signOut' },
+          ],
+        },
+      ];
+    },
+
     processing () {
       return this.loadingUser || this.loadingRoom || !this.input || this.updating;
     },
@@ -75,6 +95,7 @@ export default {
 
     ...mapState([
       'loadingUser',
+      'userName',
     ]),
 
     ...mapState('rooms', [
