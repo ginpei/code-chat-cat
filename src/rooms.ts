0115658308943d259dc780402d444c798766b162
ginpei <ginpei@gmail.com>
2019-02-17 17:01:21 -0800

    connect room to firebase

--- src/rooms.ts
+++ src/rooms.ts
@@ -13,3 +13,46 @@ export async function loadRooms (user: firebase.User): Promise<IRoom[]> {
     };
   });
 }
+
+export async function loadRoom (user: firebase.User, roomId: string): Promise<IRoom | null> {
+  const snapshot = await firebase.firestore().collection('/rooms').doc(roomId).get();
+  const data = snapshot.data();
+  if (!data) {
+    return null;
+  }
+
+  return {
+    id: roomId,
+    name: data.name,
+    textbookContent: data.textbookContent,
+  };
+}
+
+export async function updateRoom (room: IRoom): Promise<void> {
+  const data = {
+    name: room.name,
+    textbookContent: room.textbookContent,
+  };
+  return firebase.firestore().collection('/rooms').doc(room.id).update(data);
+}
+
+export function observeRoom (id: string, observer: (room: IRoom | null) => void): () => void {
+  const ref = firebase.firestore().collection('/rooms').doc(id);
+  return ref.onSnapshot((snapshot) => {
+    const room = snapshotToRoom(snapshot);
+    observer(room);
+  });
+}
+
+function snapshotToRoom (snapshot: firebase.firestore.DocumentSnapshot): IRoom | null {
+  const data = snapshot.data();
+  if (!data) {
+    return null;
+  }
+
+  return {
+    id: snapshot.id,
+    name: data.name,
+    textbookContent: data.textbookContent,
+  };
+}
--- src/screens/RoomTextbookPage.tsx
+++ src/screens/RoomTextbookPage.tsx
@@ -4,6 +4,8 @@ import { RouteComponentProps } from 'react-router-dom';
 import styled from 'styled-components';
 import Markdown from '../components/Markdown';
 import { Dispatch, IState } from '../reducers';
+import { IRoom, RoomsActionTypes } from '../reducers/rooms';
+import { observeRoom, updateRoom } from '../rooms';
 
 const headerHeight = 12 * 1.6;
 
@@ -24,45 +26,86 @@ const EditorContainer = styled.div`
   height: calc(100vh - ${headerHeight}px);
 `;
 const EditorInput = styled.textarea`
-  background-color: #eee;
+  background-color: #f0f0f0;
   border-style: none;
   overflow-y: scroll;
-  padding: 0.4rem;
+  padding: 1rem;
   resize: none;
 `;
 const EditorOutput = styled.article`
   overflow-y: scroll;
-  padding: 0.4rem;
+  padding: 1rem;
 `;
+
+enum PageStatus {
+  initial,
+  loading,
+  ready,
+}
 
 interface IRoomTextbookPageParams {
   id: string;
 }
 interface IRoomTextbookPageProps
   extends RouteComponentProps<IRoomTextbookPageParams> {
+  firebaseUser: firebase.User | null;
   loggedIn: boolean;
+  setRooms: (rooms: IRoom[]) => void;
   userName: string;
 }
 
 function RoomTextbookPage (props: IRoomTextbookPageProps) {
+  if (!props.firebaseUser) {
+    return (
+      <div>
+        <p>401</p>
+      </div>
+    );
+  }
+
   const roomId = props.match.params.id;
-  const [state, update] = useState({
-    text: '# Hello World!\n\nEdit here!',
-  });
+  const [pageStatus, setPageStatus] = useState(PageStatus.initial);
+  const [room, setRoom] = useState<IRoom | null>(null);
+
+  if (pageStatus === PageStatus.initial) {
+    // TODO unobserve when unmount
+    const unobserve = observeRoom(roomId, (updatedRoom) => {
+      setRoom(updatedRoom);
+      setPageStatus(PageStatus.ready);
+    });
+
+    setPageStatus(PageStatus.loading);
+    return (
+      <div title="Initializing room">...</div>
+    );
+  }
+
+  if (pageStatus === PageStatus.ready && !room) {
+    return (
+      <div>
+        <p>404</p>
+      </div>
+    );
+  }
 
   const onChangeInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    return update({ text: event.currentTarget.value });
+    if (!room) { return; }
+    const textbookContent = event.currentTarget.value;
+    updateRoom({ ...room, textbookContent });
   };
+
+  const roomName = room ? room.name : '';
+  const content = room ? room.textbookContent : '';
 
   return (
     <div>
       <Header>
-        <AppName>Room {roomId}</AppName>
+        <AppName>{roomName}</AppName>
       </Header>
       <EditorContainer>
-        <EditorInput onChange={onChangeInput}>{state.text}</EditorInput>
+        <EditorInput onChange={onChangeInput} value={content} disabled={!content}/>
         <EditorOutput>
-          <Markdown content={state.text} />
+          <Markdown content={content} />
         </EditorOutput>
       </EditorContainer>
     </div>
@@ -70,11 +113,17 @@ function RoomTextbookPage (props: IRoomTextbookPageProps) {
 }
 
 const mapStateToProps = (state: IState) => ({
+  firebaseUser: state.currentUser.firebaseUser,
   loggedIn: state.currentUser.loggedIn,
   userName: state.currentUser.name,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({});
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  setRooms: (rooms: IRoom[]) => dispatch({
+    rooms,
+    type: RoomsActionTypes.setRooms,
+  }),
+});
 
 export default connect(
   mapStateToProps,
