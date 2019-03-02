01bd521921c6ea1c176902e8282d29cbeecc6c48
ginpei <ginpei@gmail.com>
2019-03-02 01:50:08 -0800

    delete old files

--- _beta1/README.md
+++
@@ -1,3 +0,0 @@
-# Code Chat Cat
-
-![assets/logo-512.png](assets/logo-512.png)
--- _beta1/plugins/firebase.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  apiKey: 'AIzaSyDu7McDrMD0wS2PSPoZzfHoujr0lD4VAUk',
-  authDomain: 'code-chat-cat.firebaseapp.com',
-  databaseURL: 'https://code-chat-cat.firebaseio.com/',
-  projectId: 'code-chat-cat',
-  storageBucket: 'gs://code-chat-cat.appspot.com',
-};
