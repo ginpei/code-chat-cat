3044151342154126d8fec1ad1ef575a329c4e064
ginpei <ginpei@gmail.com>
2019-03-12 13:18:10 -0700

    merge beta-2

--- types/html-to-react.d.ts
+++
@@ -1,6 +0,0 @@
-export namespace IsValidNodeDefinitions {
-  function alwaysValid(): any;
-}
-export function Parser(options: any): any;
-export function ProcessNodeDefinitions(): any;
-export function ProcessingInstructions(): any;
