122b9e08e30a96843d7f962a8a0bf329d0c01b9f
ginpei <ginpei@gmail.com>
2019-04-23 01:14:32 -0700

    remoe migration system

--- src/misc.ts
+++ src/misc.ts
@@ -1,4 +1,3 @@
-import firebase from 'firebase';
 import { createBrowserHistory } from 'history';
 
 export const noop: () => void = () => undefined;
@@ -8,51 +7,6 @@ export function sleep (ms: number) {
 }
 
 export const appHistory = createBrowserHistory();
-
-export interface IRecord {
-  createdAt: firebase.firestore.Timestamp;
-  updatedAt: firebase.firestore.Timestamp;
-  modelVersion: string;
-}
-
-export type ClientRecord<T> = {
-  [P in Exclude<keyof T, 'modelVersion'>]: T[P];
-} & { id: string; };
-
-export type Migrations = { [key in string]: (v: any) => any };
-
-export function isRecord (record: any): record is IRecord {
-  return 'createdAt' in record && 'updatedAt' in record && 'modelVersion' in record;
-}
-
-// TODO remove default (wow)
-export default function migrate (migrations: Migrations, record: any) {
-  const startedAt = Date.now();
-  while (true) {
-    // most likely infinite loop
-    if (Date.now() - startedAt > 3000) {
-      throw new Error('Timed out');
-    }
-
-    if (!isRecord(record)) {
-      console.warn(record);
-      throw new Error('Invalid record data');
-    }
-
-    const { modelVersion } = record;
-    const proceed = migrations[modelVersion];
-    if (!proceed) {
-      throw new Error(`Migration not found: ${modelVersion}`);
-    }
-
-    const result = proceed(record);
-    if (result.modelVersion === record.modelVersion) {
-      return result;
-    }
-
-    record = result;
-  }
-}
 
 // tslint:disable-next-line:ban-types
 export function debounce<F extends Function> (fn: F, delay: number): F {
